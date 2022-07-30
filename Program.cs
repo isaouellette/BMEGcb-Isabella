f29175c5db479913f25f8c3f2a0587e817e90247
isaouellette <109050798+isaouellette@users.noreply.github.com>
2022-07-30 15:07:52 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace BCBTwo
+{ // The namespace will default to project/solution name
+    class ProgramOne
+    {
+        static void Main()
+        {
+            int val1 = 2;
+            Console.WriteLine("Hello World!");
+            int val2 = 7;
+            Console.WriteLine("value three: {0} ", val1 + val2);
+        }
+    }
+}
